60147e673e4d99085ce55b7b31b0b25e442ecef2
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2023-11-06 13:23:37 +0100

    feat: fullscreen

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowMode};
 
 mod boids_plugin;
 
 fn main() {
+    let window_plugin = WindowPlugin {
+        primary_window: Some(Window {
+            mode: WindowMode::Fullscreen,
+            ..default()
+        }),
+        ..default()
+    };
+
     App::new()
-        .add_plugins((DefaultPlugins, boids_plugin::BoidsPlugin))
+        .add_plugins((DefaultPlugins.set(window_plugin), boids_plugin::BoidsPlugin))
         .run();
 }
